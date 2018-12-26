943388ffd24b1c438a3a171428e49558a66a546a
Katsuya003 <bb.win.3@gmail.com>
2018-12-26 21:11:20 +0900

    [add] Add style association.

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,32 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, './dist')
-  }
-}
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            }
+          }
+        ],
+      },
+      {
+        test: /\.(gif|png|jpg|eot|wof|woff|woff2|ttf|svg)$/,
+        loader: 'url-loader'
+      }
+    ],
+  },
+};
