1c427e24e771cdbe7e9e86985cc734055ee5b4f0
Katsuya003 <bb.win.3@gmail.com>
2018-12-27 20:58:43 +0900

    [add] Add countdown app.

--- src/countdown.js
+++ src/countdown.js
@@ -1,4 +1,18 @@
 import './assets/style.scss'
+import anime from 'animejs'
 
-const sample = document.getElementById('sample')
-console.log('testt')
+const obj = {num: '0'}
+
+document.addEventListener('click', () => {
+  anime({
+    targets: obj,
+    num: 1000,
+    round: 1,
+    easing: 'linear',
+    duration: 3000,
+    update: () => {
+      const el = document.getElementsByClassName('countdown')
+      el[0].innerHTML = obj['num']
+    }
+  })
+})
